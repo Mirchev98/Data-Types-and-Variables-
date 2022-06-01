c97136b0d2bd4207f3d1b07820932deb1d151a29
Mircho Mirchev <Mircho.Mirchev98@gmail.com>
2022-06-01 21:45:57 +0300

    Pushing some solved problems

---
+++ Data Types And Variables Lab/2. Pounds to Dollars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2._Pounds_to_Dollars
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            int pounds = int.Parse(Console.ReadLine());
+
+            double poundsToUsRate = 1.31;
+
+            double dollars = pounds * poundsToUsRate;
+            
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}
---
+++ Data Types And Variables Lab/3. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _3._Exact_Sum_of_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal numbers = decimal.Parse(Console.ReadLine());
+
+            decimal sum = 0;
+
+            for (int i = 1; i <= numbers; i++)
+            {
+                decimal inputNumber = decimal.Parse(Console.ReadLine());
+                sum += inputNumber;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
